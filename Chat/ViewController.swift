43952639ef4166e50a0b93ed484c76bee4c66249
AlijonJumayev <alijon.jumayev91@gmail.com>
2024-04-02 08:57:43 +0500

    Initial Commit

---
+++ Chat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chat
+//
+//  Created by Alijon Jumayev on 4/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
